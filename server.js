20eae5ac43d1aab4255df5759f201bea6b93518b
AnimawebSolutions <animaweb@gmail.com>
2026-05-18 12:34:29 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/pixel", (req, res) => {
+
+    const ip =
+        req.headers["x-forwarded-for"] ||
+        req.socket.remoteAddress;
+
+    console.log("IP:", ip);
+
+    res.sendFile(path.join(__dirname, "pixel.png"));
+});
+
+app.listen(3000, () => {
+    console.log("Running");
+});
